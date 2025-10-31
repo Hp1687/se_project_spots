f2984f25ec003c686767a16a8a7661c867b7262c
Hp1687 <harrypatel1687@gmail.com>
2025-10-31 12:12:25 -0500

    Update HTML, JS, and popup.css with smooth modals and pre-filled profile form

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,46 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileCloseBtn = editProfileModal.querySelector(".popup__close-btn");
+const editProfileForm = editProfileModal.querySelector(".popup__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostCloseBtn = newPostModal.querySelector(".popup__close-btn");
 
-editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+// Open Edit Profile Modal
+editProfileBtn.addEventListener("click", () => {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  editProfileModal.classList.add("popup_opened");
 });
 
-editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+// Close Edit Profile Modal
+editProfileCloseBtn.addEventListener("click", () => {
+  editProfileModal.classList.remove("popup_opened");
 });
 
-newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+// Open New Post Modal
+newPostBtn.addEventListener("click", () => {
+  newPostModal.classList.add("popup_opened");
 });
 
-newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+// Close New Post Modal
+newPostCloseBtn.addEventListener("click", () => {
+  newPostModal.classList.remove("popup_opened");
 });
+
+// Handle Edit Profile form submission
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("popup_opened");
+}
+
+// Add form listener
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
